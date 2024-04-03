5b48fb3a0e1c4ab8889c2002ffa72a275680d3dd
hvthhien <hvthhien@gmail.com>
2024-04-03 16:54:09 +0700

    add sentry to autonomy logger

--- go.mod
+++ go.mod
@@ -5,6 +5,12 @@ go 1.20
 require go.uber.org/zap v1.24.0
 
 require (
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+)
+
+require (
+	github.com/getsentry/sentry-go v0.27.0
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 )
--- logger.go
+++ logger.go
@@ -3,19 +3,31 @@ package log
 import (
 	"strings"
 
+	"github.com/getsentry/sentry-go"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
 var defaultLogger *zap.Logger
 
-func Initialize(level string, isDebug bool) error {
+func Initialize(level string, isDebug bool, sentryDsn string) error {
 	log, err := New(level, isDebug)
 	if err != nil {
 		return err
 	}
 
 	defaultLogger = log
+
+	// Init sentry
+	if sentryDsn != "" {
+		err := sentry.Init(sentry.ClientOptions{
+			Dsn: sentryDsn,
+		})
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
@@ -60,14 +72,38 @@ func Debug(msg string, fields ...zap.Field) {
 
 func Info(msg string, fields ...zap.Field) {
 	MustDefaultLogger().WithOptions(zap.AddCallerSkip(1)).Info(msg, fields...)
+
+	// Add a breadcrumb
+	sentry.AddBreadcrumb(&sentry.Breadcrumb{
+		Message: msg,
+		Level:   sentry.LevelInfo,
+		Data:    zapFieldsToMap(fields),
+	})
 }
 
 func Warn(msg string, fields ...zap.Field) {
 	MustDefaultLogger().WithOptions(zap.AddCallerSkip(1)).Warn(msg, fields...)
+
+	// Add a breadcrumb
+	sentry.AddBreadcrumb(&sentry.Breadcrumb{
+		Message: msg,
+		Level:   sentry.LevelWarning,
+		Data:    zapFieldsToMap(fields),
+	})
 }
 
 func Error(msg string, fields ...zap.Field) {
 	MustDefaultLogger().WithOptions(zap.AddCallerSkip(1)).Error(msg, fields...)
+
+	// Add a breadcrumb
+	sentry.AddBreadcrumb(&sentry.Breadcrumb{
+		Message: msg,
+		Level:   sentry.LevelError,
+		Data:    zapFieldsToMap(fields),
+	})
+
+	// Capture the error with Sentry
+	sentry.CaptureMessage(msg)
 }
 
 func Panic(msg string, fields ...zap.Field) {
@@ -84,4 +120,20 @@ func DefaultLogger() *zap.Logger {
 
 func Sugar() *zap.SugaredLogger {
 	return MustDefaultLogger().Sugar()
+}
+
+// Convert zap fields to a map that Sentry can understand
+func zapFieldsToMap(fields []zap.Field) map[string]interface{} {
+	data := make(map[string]interface{})
+	for _, field := range fields {
+		var value interface{}
+		encoder := zapcore.NewMapObjectEncoder()
+		field.AddTo(encoder)
+		for _, v := range encoder.Fields {
+			value = v
+		}
+
+		data[field.Key] = value
+	}
+	return data
 }
