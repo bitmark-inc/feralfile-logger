75e131a14998a28c56bfd8f1db273db80d94ca29
Hiếu Phạm <hieupham@bitmark.com>
2025-03-18 18:09:24 +0700

    refactor and support cadence logger

--- logger.go
+++ logger.go
@@ -2,24 +2,25 @@ package log
 
 import (
 	"context"
-	"strings"
 
 	"github.com/getsentry/sentry-go"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-var defaultLogger *zap.Logger
+var logger *zap.Logger
 
-func Initialize(level string, isDebug bool, sentryOptions *sentry.ClientOptions) error {
-	log, err := newLogger(level, isDebug)
+// Initialize creates a new logger
+func Initialize(isDebug bool, sentryOptions *sentry.ClientOptions) error {
+	// init logger
+	log, err := newLogger(isDebug)
 	if err != nil {
 		return err
 	}
 
-	defaultLogger = log
+	logger = log
 
-	// Init sentry
+	// init sentry
 	if sentryOptions != nil {
 		err := sentry.Init(*sentryOptions)
 		if err != nil {
@@ -30,9 +31,9 @@ func Initialize(level string, isDebug bool, sentryOptions *sentry.ClientOptions)
 	return nil
 }
 
-func newLogger(level string, isDebug bool) (*zap.Logger, error) {
+// newLogger creates a new logger
+func newLogger(isDebug bool) (*zap.Logger, error) {
 	var config zap.Config
-
 	if isDebug {
 		config = zap.NewDevelopmentConfig()
 		config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
@@ -41,38 +42,40 @@ func newLogger(level string, isDebug bool) (*zap.Logger, error) {
 		config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 	}
 
-	// override log level by configuration
-	l := zap.ErrorLevel
-	switch strings.ToUpper(level) {
-	case "TRACE", "DEBUG":
-		l = zap.DebugLevel
-	case "INFO":
-		l = zap.InfoLevel
-	case "WARN":
-		l = zap.WarnLevel
-	}
-
-	config.Level = zap.NewAtomicLevelAt(l)
-
 	return config.Build()
 }
 
+// Logger returns the logger
+func Logger() *zap.Logger {
+	if logger == nil {
+		panic("logger not initialized")
+	}
+	return logger
+}
+
+// Debug logs a message at Debug level.
 func Debug(msg string, fields ...zap.Field) {
-	DefaultLogger().WithOptions(zap.AddCallerSkip(1)).Debug(msg, fields...)
+	ldebug(Logger(), msg, hub(), fields...)
 }
 
+func ldebug(logger *zap.Logger, msg string, hub *sentry.Hub, fields ...zap.Field) {
+	logger.WithOptions(zap.AddCallerSkip(1)).Debug(msg, fields...)
+}
+
+// Info logs a message at Info level.
 func Info(msg string, fields ...zap.Field) {
-	info(msg, hub(), fields...)
+	linfo(Logger(), msg, hub(), fields...)
 }
 
+// InfoWithContext logs a message at Info level with a context.
 func InfoWithContext(ctx context.Context, msg string, fields ...zap.Field) {
-	info(msg, hubOnContext(ctx), fields...)
+	linfo(Logger(), msg, hubOnContext(ctx), fields...)
 }
 
-func info(msg string, hub *sentry.Hub, fields ...zap.Field) {
-	DefaultLogger().WithOptions(zap.AddCallerSkip(1)).Info(msg, fields...)
+func linfo(logger *zap.Logger, msg string, hub *sentry.Hub, fields ...zap.Field) {
+	logger.WithOptions(zap.AddCallerSkip(1)).Info(msg, fields...)
 
-	// Add a breadcrumb
+	// add a breadcrumb
 	addBreadcrumb(hub, &sentry.Breadcrumb{
 		Message: msg,
 		Level:   sentry.LevelInfo,
@@ -80,18 +83,20 @@ func info(msg string, hub *sentry.Hub, fields ...zap.Field) {
 	})
 }
 
+// Warn logs a message at Warning level.
 func Warn(msg string, fields ...zap.Field) {
-	warn(msg, hub(), fields...)
+	lwarn(Logger(), msg, hub(), fields...)
 }
 
+// Warn logs a message at Warning level.
 func WarnWithContext(ctx context.Context, msg string, fields ...zap.Field) {
-	warn(msg, hubOnContext(ctx), fields...)
+	lwarn(Logger(), msg, hubOnContext(ctx), fields...)
 }
 
-func warn(msg string, hub *sentry.Hub, fields ...zap.Field) {
-	DefaultLogger().WithOptions(zap.AddCallerSkip(1)).Warn(msg, fields...)
+func lwarn(logger *zap.Logger, msg string, hub *sentry.Hub, fields ...zap.Field) {
+	logger.WithOptions(zap.AddCallerSkip(1)).Warn(msg, fields...)
 
-	// Add a breadcrumb
+	// add a breadcrumb
 	addBreadcrumb(hub, &sentry.Breadcrumb{
 		Message: msg,
 		Level:   sentry.LevelWarning,
@@ -99,27 +104,90 @@ func warn(msg string, hub *sentry.Hub, fields ...zap.Field) {
 	})
 }
 
+// Error logs a message at Error level.
 func Error(msg string, fields ...zap.Field) {
-	err(msg, hub(), fields...)
+	lerror(Logger(), msg, hub(), fields...)
 }
 
+// ErrorWithContext logs a message at Error level with a context.
 func ErrorWithContext(ctx context.Context, msg string, fields ...zap.Field) {
-	err(msg, hubOnContext(ctx), fields...)
+	lerror(Logger(), msg, hubOnContext(ctx), fields...)
 }
 
-func err(msg string, hub *sentry.Hub, fields ...zap.Field) {
-	DefaultLogger().WithOptions(zap.AddCallerSkip(1)).Error(msg, fields...)
+func lerror(logger *zap.Logger, msg string, hub *sentry.Hub, fields ...zap.Field) {
+	logger.WithOptions(zap.AddCallerSkip(1)).Error(msg, fields...)
 
-	// Add a breadcrumb
+	// add a breadcrumb
 	addBreadcrumb(hub, &sentry.Breadcrumb{
 		Message: msg,
 		Level:   sentry.LevelError,
 		Data:    zapFieldsToMap(fields),
 	})
 
+	// capture message
 	captureMessage(msg, hub)
 }
 
+// Panic logs a message at Panic level.
+func Panic(msg string, fields ...zap.Field) {
+	lpanic(Logger(), msg, hub(), fields...)
+}
+
+// PanicWithContext logs a message at Panic level with a context.
+func PanicWithContext(ctx context.Context, msg string, fields ...zap.Field) {
+	lpanic(Logger(), msg, hubOnContext(ctx), fields...)
+}
+
+func lpanic(logger *zap.Logger, msg string, hub *sentry.Hub, fields ...zap.Field) {
+	// add a breadcrumb
+	addBreadcrumb(hub, &sentry.Breadcrumb{
+		Message: msg,
+		Level:   sentry.LevelFatal,
+		Data:    zapFieldsToMap(fields),
+	})
+
+	// capture message
+	captureMessage(msg, hub)
+
+	// panic
+	logger.WithOptions(zap.AddCallerSkip(1)).Panic(msg, fields...)
+}
+
+// Fatal logs a message at Fatal level.
+func Fatal(msg string, fields ...zap.Field) {
+	lfatal(Logger(), msg, hub(), fields...)
+}
+
+// FatalWithContext logs a message at Fatal level with a context.
+func FatalWithContext(ctx context.Context, msg string, fields ...zap.Field) {
+	lfatal(Logger(), msg, hubOnContext(ctx), fields...)
+}
+
+func lfatal(logger *zap.Logger, msg string, hub *sentry.Hub, fields ...zap.Field) {
+	// add a breadcrumb
+	addBreadcrumb(hub, &sentry.Breadcrumb{
+		Message: msg,
+		Level:   sentry.LevelFatal,
+		Data:    zapFieldsToMap(fields),
+	})
+
+	// capture message
+	captureMessage(msg, hub)
+
+	logger.WithOptions(zap.AddCallerSkip(1)).Fatal(msg, fields...)
+}
+
+// Sugar returns a sugared Logger().
+func Sugar() *zap.SugaredLogger {
+	return Logger().Sugar()
+}
+
+// Sync flushes any buffered log entries.
+func Sync() error {
+	return Logger().Sync()
+}
+
+// addBreadcrumb adds a breadcrumb to Sentry
 func addBreadcrumb(hub *sentry.Hub, breadcrumb *sentry.Breadcrumb) {
 	if nil != hub {
 		hub.AddBreadcrumb(breadcrumb, nil)
@@ -128,6 +196,7 @@ func addBreadcrumb(hub *sentry.Hub, breadcrumb *sentry.Breadcrumb) {
 	}
 }
 
+// captureMessage captures a message to Sentry
 func captureMessage(msg string, hub *sentry.Hub) {
 	if nil != hub {
 		hub.CaptureMessage(msg)
@@ -136,30 +205,7 @@ func captureMessage(msg string, hub *sentry.Hub) {
 	}
 }
 
-func Panic(msg string, fields ...zap.Field) {
-	DefaultLogger().WithOptions(zap.AddCallerSkip(1)).Panic(msg, fields...)
-}
-
-func Fatal(msg string, fields ...zap.Field) {
-	DefaultLogger().WithOptions(zap.AddCallerSkip(1)).Fatal(msg, fields...)
-}
-
-func Sugar() *zap.SugaredLogger {
-	return DefaultLogger().Sugar()
-}
-
-func Sync() error {
-	return DefaultLogger().Sync()
-}
-
-func DefaultLogger() *zap.Logger {
-	if defaultLogger == nil {
-		panic("use logger without initializing")
-	}
-
-	return defaultLogger
-}
-
+// hubOnContext returns a hub from the context, creating a new one if it doesn't exist
 func hubOnContext(ctx context.Context) *sentry.Hub {
 	hub := sentry.GetHubFromContext(ctx)
 	if hub == nil {
@@ -169,11 +215,12 @@ func hubOnContext(ctx context.Context) *sentry.Hub {
 	return hub
 }
 
+// hub returns the current hub
 func hub() *sentry.Hub {
 	return sentry.CurrentHub()
 }
 
-// Convert zap fields to a map that Sentry can understand
+// zapFieldsToMap converts zap fields to a map that Sentry can understand
 func zapFieldsToMap(fields []zap.Field) map[string]interface{} {
 	data := make(map[string]interface{})
 	for _, field := range fields {
--- wrapper.go
+++ wrapper.go
@@ -1,8 +1,11 @@
 package log
 
 import (
+	"context"
 	"fmt"
 
+	"go.uber.org/cadence/activity"
+	"go.uber.org/cadence/workflow"
 	"go.uber.org/zap"
 )
 
@@ -10,9 +13,85 @@ type cloudflareLogger struct {
 }
 
 func (l *cloudflareLogger) Printf(format string, args ...any) {
-	DefaultLogger().Info(fmt.Sprintf(format, args...), zap.Any("args", args))
+	Logger().Info(fmt.Sprintf(format, args...), zap.Any("args", args))
 }
 
 func CloudflareLogger() *cloudflareLogger {
 	return &cloudflareLogger{}
+}
+
+type cadenceWorkflowLogger struct {
+	ctx    workflow.Context
+	logger *zap.Logger
+}
+
+func CadenceWorkflowLogger(ctx workflow.Context) *cadenceWorkflowLogger {
+	return &cadenceWorkflowLogger{
+		ctx:    ctx,
+		logger: workflow.GetLogger(ctx),
+	}
+}
+
+func (l *cadenceWorkflowLogger) attachInfo(fields ...zap.Field) []zap.Field {
+	fields = append(fields, zap.String("workflow_id", workflow.GetInfo(l.ctx).WorkflowExecution.ID))
+	fields = append(fields, zap.String("workflow_type", workflow.GetInfo(l.ctx).WorkflowType.Name))
+	fields = append(fields, zap.String("workflow_run_id", workflow.GetInfo(l.ctx).WorkflowExecution.RunID))
+	return fields
+}
+
+func (l *cadenceWorkflowLogger) Debug(msg string, fields ...zap.Field) {
+	ldebug(l.logger, msg, hub(), l.attachInfo(fields...)...)
+}
+
+func (l *cadenceWorkflowLogger) Info(msg string, fields ...zap.Field) {
+	linfo(l.logger, msg, hub(), l.attachInfo(fields...)...)
+}
+
+func (l *cadenceWorkflowLogger) Warn(msg string, fields ...zap.Field) {
+	lwarn(l.logger, msg, hub(), l.attachInfo(fields...)...)
+}
+
+func (l *cadenceWorkflowLogger) Error(msg string, fields ...zap.Field) {
+	lerror(l.logger, msg, hub(), l.attachInfo(fields...)...)
+}
+
+type cadenceActivityLogger struct {
+	ctx    context.Context
+	logger *zap.Logger
+}
+
+func CadenceActivityLogger(ctx context.Context) *cadenceActivityLogger {
+	return &cadenceActivityLogger{
+		ctx:    ctx,
+		logger: activity.GetLogger(ctx),
+	}
+}
+
+func (l *cadenceActivityLogger) attachInfo(fields ...zap.Field) []zap.Field {
+	fields = append(fields, zap.String("workflow_id", activity.GetInfo(l.ctx).WorkflowExecution.ID))
+	fields = append(fields, zap.String("workflow_run_id", activity.GetInfo(l.ctx).WorkflowExecution.RunID))
+	fields = append(fields, zap.String("activity_id", activity.GetInfo(l.ctx).ActivityID))
+	fields = append(fields, zap.String("activity_type", activity.GetInfo(l.ctx).ActivityType.Name))
+	wft := activity.GetInfo(l.ctx).WorkflowType
+	if wft != nil {
+		fields = append(fields, zap.String("workflow_type", wft.Name))
+	}
+
+	return fields
+}
+
+func (l *cadenceActivityLogger) Debug(msg string, fields ...zap.Field) {
+	ldebug(l.logger, msg, hub(), l.attachInfo(fields...)...)
+}
+
+func (l *cadenceActivityLogger) Info(msg string, fields ...zap.Field) {
+	linfo(l.logger, msg, hub(), l.attachInfo(fields...)...)
+}
+
+func (l *cadenceActivityLogger) Warn(msg string, fields ...zap.Field) {
+	lwarn(l.logger, msg, hub(), l.attachInfo(fields...)...)
+}
+
+func (l *cadenceActivityLogger) Error(msg string, fields ...zap.Field) {
+	lerror(l.logger, msg, hub(), l.attachInfo(fields...)...)
 }
